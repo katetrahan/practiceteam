0896a25e1d098a9489cbb495a53af9835437974d
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-01-12 10:21:14 -0800

    add first test of TeamTest: team instantiates

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -1,10 +1,29 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TeamTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Team.clearAll();
+    }
+
+    @Test
+    public void team_teamInstantiatesCorrectly_String() throws Exception {
+        Member newMember = new Member("Oprah", "Winfrey", "Public Figure");
+        Team team = new Team("Hackers", "Java Course Team", newMember);
+        assertEquals(true, team instanceof Team);
+    }
+
 
 
 
