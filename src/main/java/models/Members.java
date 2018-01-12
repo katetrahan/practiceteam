28ebae6189e4667e5907c846da4edf1e051d008a
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-01-12 09:22:00 -0800

    set up members and team java files

--- src/main/java/models/Members.java
+++ src/main/java/models/Members.java
@@ -1,5 +1,17 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Members {
+    private String name;
+    private String description;
+    private static ArrayList<Members> instances = new ArrayList<>();
+    private int memberId;
+    public Members(String name, String description){
+        this.name = name;
+        this.description = description;
+        instances.add(this);
+        this.memberId = instances.size();
+    }
 
 }
--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,5 +1,14 @@
 package models;
 
 public class Team {
+    private String name;
+    private String description;
+
+
+    public Team(String name, String description){
+        this.name = name;
+        this.description = description;
+
+    }
 
 }
